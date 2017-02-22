3aac9bb971d009bcff185d31badf93a2cc1896b6
Aditya Logishetty <adi.logishetty@gmail.com>
2017-02-21 19:05:28 -0500

    Add files via upload

---
+++ dev_project_1/mystyle.js
@@ -0,0 +1,4 @@
+
+function myFunction() {
+    document.getElementById("obj").innerHTML = "Graduate in Information Systems Management actively seeking summer internships to better enhance my skills. An aspiring individual aiming to help achieve company goals and objectives.";
+}
